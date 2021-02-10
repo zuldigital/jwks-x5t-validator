c49dfb4654b516d91f400e0f006b70792b7d7930
Marcelo Liberato <mliberato@gmail.com>
2021-02-10 14:37:30 -0300

    documenting steps to compute x5t

--- validate-x5t.sh
+++ validate-x5t.sh
@@ -5,6 +5,7 @@ JKU="$1"
 echo "Validating x5t from JWKS at: $JKU"
 echo
 
+# retrieve all keys (one kid/x5t/x5c per line) from JWKS
 list=`curl -s $1 | jq --raw-output '.keys[] | ("\(.kid) \(.x5t) \(.x5c[0])")'`
 
 while IFS= read -r line; do
@@ -12,6 +13,11 @@ while IFS= read -r line; do
   x5t=`echo $line | awk '{print $2}'`
   x5c=`echo $line | awk '{print $3}'`
 
+  # for each line compute x5t by following these steps:
+  #  1. "echo $x5c|base64 -d" -> base64 decode x5c certificate
+  #  2. "openssl sha1 -binary" -> compute certificate's SHA-1 in binary format
+  #  3. "base64" -> base64 encode it
+  #  4. "tr -d '='|tr '/+' '_-'" -> convert from base64 to base64url as demanded by RFC7515: https://tools.ietf.org/html/rfc7515#page-12
   computed_x5t=`echo $x5c|base64 -d|openssl sha1 -binary|base64|tr -d '='|tr '/+' '_-'`
 
   echo "  kid: $kid"
